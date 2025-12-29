adc44f33e769efe15c84d00c98aaa0ce55b313ad
brianpimentel <brianepimentel@gmail.com>
2025-12-29 12:46:17 -0500

    Revert "Init"
    This reverts commit 9ab14bf1380c275f62997d5ec63ceff37be9a944.

--- Source/WallRunning/WallRunning.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "WallRunning.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WallRunning, "WallRunning" );
- 
